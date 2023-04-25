7e1fe2244c37830b913df9bbc8847fce6832e399
angelith <theodosios.angelidis@protonmail.com>
2023-04-26 00:55:17 +0300

    Enabled caching.

--- src/main/java/com/example/sakis/spring/rest/api/postgresql/RestApiPostgresApplication.java
+++ src/main/java/com/example/sakis/spring/rest/api/postgresql/RestApiPostgresApplication.java
@@ -2,9 +2,11 @@ package com.example.sakis.spring.rest.api.postgresql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 
 @SpringBootApplication
+@EnableCaching
 public class RestApiPostgresApplication {
 
     public static void main(String[] args) {
